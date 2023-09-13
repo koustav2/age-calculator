9194d2840bc00dea7a061af2201690ecf86f9a96
koustav <koustavm.aeie2018@nsec.ac.in>
2023-09-13 20:09:12 +0530

    commit -2

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,27 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    fontFamily: {
+      poppins: ['Poppins', 'sans-serif'],
+    },
+    backgroundColor: {
+      'p1': 'hsl(259, 100%, 65%)',
+      'p2': 'hsl(0, 100%, 67%)',
+      'g1': 'hsl(0, 0%, 86%)',
+      'g2': 'hsl(0, 1%, 44%)',
+      'b1': 'hsl(0, 0%, 8%)',
+      'w1': 'hsl(0, 0%, 100%)',
+      'w2': 'hsl(0, 0%, 94%)',
+    },
+    textColor: {
+      'p1': 'hsl(259, 100%, 65%)',
+      'p2': 'hsl(0, 100%, 67%)',
+      'g1': 'hsl(0, 0%, 86%)',
+      'g2': 'hsl(0, 1%, 44%)',
+      'b1': 'hsl(0, 0%, 8%)',
+      'w1': 'hsl(0, 0%, 100%)',
+      'w2': 'hsl(0, 0%, 94%)',
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
